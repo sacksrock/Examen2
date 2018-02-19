fb7d8339d45dd2fef2a8f0e6aad5b09bbd8345f3
sacksrock <gradomedio2smr@gmail.com>
2018-02-19 11:19:01 +0100

    Arreglos en el mainfest login y registro firebase funcionando 100%

--- app/src/main/java/com/utad/danieliglesia/examenpmdm_dint/SecondActivity.java
+++ app/src/main/java/com/utad/danieliglesia/examenpmdm_dint/SecondActivity.java
@@ -1,8 +1,46 @@
 package com.utad.danieliglesia.examenpmdm_dint;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.google.firebase.database.DataSnapshot;
+
 /**
  * Created by daniel.iglesia on 19/02/2018.
  */
 
-public class SecondActivity {
+public class SecondActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        SecondActivityEvents events = new SecondActivityEvents(this);
+        DataHolder.instance.fireBaseAdmin.setListener(events);
+
+    }
+
+    class SecondActivityEvents implements FireBaseAdminListener {
+
+        SecondActivity secondActivity;
+
+
+        public SecondActivityEvents(SecondActivity secondActivity) {
+            this.secondActivity = secondActivity;
+        }
+
+
+        @Override
+        public void FireBaseAdmin_RegisterOk(Boolean ok) {
+
+        }
+
+        @Override
+        public void FireBaseAdmin_LoginOk(Boolean ok) {
+
+        }
+
+        @Override
+        public void FireBaseAdmin_RamaDescargada(String rama, DataSnapshot dataSnapshot) {
+
+        }
+    }
 }
